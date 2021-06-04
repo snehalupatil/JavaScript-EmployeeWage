89cd949ea87f618bce29948737f7d2a846f3add8
snehalupatil <patilsnehal4575@gmail.com>
2021-06-04 17:48:48 +0530

    Added: Daily EMployee Wage using Switch Case

--- employeeWage.js
+++ employeeWage.js
@@ -12,3 +12,27 @@ else{
 }
 
 
+//UC2: Calculte Daily Employee Wage using switch case
+
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+let empHrs = 0;
+empCheck = Math.floor(Math.random() * 10) % 3;
+switch(empCheck){
+  case IS_PART_TIME:
+	  empHrs = PART_TIME_HOURS;
+	  break;
+
+	case IS_FULL_TIME:
+		empHrs = FULL_TIME_HOURS;
+		break;
+		
+	default:
+		empHrs = 0;
+}
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Daily Employee Wage: " +empWage);  
