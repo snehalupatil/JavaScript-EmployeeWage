77caccf38444c7a3f2f6c8eb416552ff5f8760cd
snehalupatil <patilsnehal4575@gmail.com>
2021-06-08 22:01:28 +0530

    Added: Store Day and Daily wage along with Total Wage

--- employeeWage.js
+++ employeeWage.js
@@ -1,5 +1,6 @@
 console.log("**Welcome to Java Script EmployeeWage**");
 
+/*
 //UC1: Check employee is absent or present
 
 const IS_ABSENT = 0;
@@ -167,3 +168,53 @@ function totalDaysWorked(numofDays, dailyWage){
 }
 	
 	console.log("UC 7G - Number of Days Emp Worked: "+ empDailyWageArr.reduce(totalDaysWorked,0));
+
+*/
+//UC8:Store Day and DailyWage along total wage using map 
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const MAX_HRS_IN_MONTH = 160;
+const NUM_OF_WORKING_DAYS = 20;
+let empHrs = 0;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+
+
+let empDailyWageArr = new Array();
+let empDailyWageMap = new Map();
+
+function calcDailyWage(empHrs){
+        return empHrs * WAGE_PER_HOUR;
+    }
+
+while (totalEmpHrs <= MAX_HRS_IN_MONTH
+	&& totalWorkingDays < NUM_OF_WORKING_DAYS){
+		totalWorkingDays++;
+		let empCheck = Math.floor(Math.random() * 10) % 3;
+		let empHrs = getWorkingHours(empCheck);
+		totalEmpHrs += empHrs;
+		empDailyWageArr.push(calcDailyWage(empHrs));
+		empDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));
+	}
+
+function getWorkingHours(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        
+         case IS_FULL_TIME:
+             return FULL_TIME_HOURS;
+              
+         default:
+             return 0;
+		}
+}
+
+console.log(empDailyWageMap);
+function totalWages(totalWage, dailyWage){
+	return totalWage + dailyWage;
+}
+console.log("UC8 - Emp Wage Map totalHrs : " + Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
