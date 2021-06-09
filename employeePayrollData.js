76d4ec3402178b288b19ccf0df4209b791f5caaa
snehalupatil <patilsnehal4575@gmail.com>
2021-06-09 10:54:25 +0530

    Added: Create Employee Payroll Data

---
+++ employeePayrollData.js
@@ -0,0 +1,26 @@
+class EmployeePayrollData{
+    //property
+    id;
+    salary;
+    
+    //constructor
+    constructor(id,name,salary){
+        this.id = id;
+        this.salary = salary;
+        this.name = name;
+    }
+
+    //getter and setter
+    get name(){return this._name;}
+    set name(name){this._name = name;}
+
+    //method
+    toString(){
+        return "id=" +this.id+ ", name=" +this.name+ ", salary=" +this.salary;
+    }
+}
+
+let employeePayrollData = new EmployeePayrollData(1,"Mark",50000);
+console.log(employeePayrollData.toString);
+employeePayrollData.name = "John";
+console.log(employeePayrollData.toString);
